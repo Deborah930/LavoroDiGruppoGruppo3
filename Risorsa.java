d42256f06bfd625c805465383161087b0ea718dd
Spatentrider <104686281+Spatentrider@users.noreply.github.com>
2025-07-04 16:13:58 +0200

    update Risorsa

---
+++ Risorsa.java
@@ -0,0 +1,120 @@
+public class Risorsa{
+    private String titolo;
+    private int annoPubblicazione;
+    private String codice;
+
+    public String getCodice() {
+        return codice;
+    }
+
+    public void setCodice(String codice) {
+        if(!this.codice.equals(codice)){
+        this.codice = codice;
+        }
+        else{
+            System.out.println("codice già esistente");
+        }
+    }
+    
+    public int getAnnoPubblicazione() {
+        return annoPubblicazione;
+    }
+
+    public void setAnnoPubblicazione(int annoPubblicazione) {
+        this.annoPubblicazione = annoPubblicazione;
+    }
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    Risorsa(String titolo, int annoPubblicazione, String codice){
+        this.titolo = titolo;
+        this.annoPubblicazione = annoPubblicazione;
+        this.codice = codice;
+    }
+
+    public void visualizzaDettagli(){
+        System.out.println("Titolo " + titolo + "anno pubblicazione: " + annoPubblicazione + " codice: " + codice);
+    }
+}
+
+class Libro extends Risorsa{
+
+    private String autore;
+    
+    public String getAutore() {
+        return autore;
+    }
+
+    public void setAutore(String autore) {
+        this.autore = autore;
+    }
+
+    Libro(String titolo, int annoPubblicazione, String codice, String autore){
+        super(titolo, annoPubblicazione, codice);
+        this.autore = autore;
+    }
+
+    @Override
+     public void visualizzaDettagli(){
+        System.out.println("Titolo " + getTitolo() + "anno pubblicazione: " + getAnnoPubblicazione() + " codice: " + getCodice() + " Autore " + getAutore());
+    }
+
+
+}
+
+class Rivista extends Risorsa{
+
+    private int numero;
+    
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    Rivista(String titolo, int annoPubblicazione, String codice, int numero){
+        super(titolo, annoPubblicazione, codice);
+        this.numero = numero;
+    }
+
+    @Override
+     public void visualizzaDettagli(){
+        System.out.println("Titolo " + getTitolo() + "anno pubblicazione: " + getAnnoPubblicazione() + " codice: " + getCodice() + " Numero della rivista " + getNumero());
+    }
+
+
+}
+
+class Ebook extends Risorsa{
+
+    String formato;
+    
+    public String getFormato() {
+        return formato;
+    }
+
+    public void setFormato(String formato) {
+        this.formato = formato;
+    }
+
+    Ebook(String titolo, int annoPubblicazione, String codice, String formato){
+        super(titolo, annoPubblicazione, codice);
+        this.formato = formato;
+    }
+
+    @Override
+     public void visualizzaDettagli(){
+        System.out.println("Titolo " + getTitolo() + "anno pubblicazione: " + getAnnoPubblicazione() + " codice: " + getCodice() + " Formato "  + getFormato());
+    }
+
+
+}
+
+
